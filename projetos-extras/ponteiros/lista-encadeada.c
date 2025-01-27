4776a2fef7b119ae36583e042e367a9fccd1dc6f
Luana Lima <luanalima.2006@hotmail.com>
2025-01-27 14:37:59 -0300

    feat: adicionar nova funções ao código de lista-encadeada

--- projetos-extras/ponteiros/lista-encadeada.c
+++ projetos-extras/ponteiros/lista-encadeada.c
@@ -6,23 +6,64 @@ typedef struct node {
   struct node *prox;
 } node;
 
-node *criarNo (int valor) {
+node *criarNo(int valor) {
   node *novo = (node *)malloc(sizeof(node));
   novo->valor = valor;
   novo->prox = NULL;
   return novo;
 }
 
-void inserir (node **topo, int valor) {
+void inserir(node **topo, int valor) {
   node *novo = criarNo(valor);
 
-  if (*topo == NULL)
+  if (*topo == NULL) {
     *topo = novo;
-  else {
+  } else {
     node *atual = *topo;
 
-    while (atual->prox != NULL) atual = atual->prox;
+    while (atual->prox != NULL)
+      atual = atual->prox;
 
     atual->prox = novo;
   }
+}
+
+void inserirOrdenado(node **topo, int valor) {
+  node *novo = criarNo(valor);
+
+  if (*topo == NULL || (*topo)->valor >= valor) {
+    novo->prox = *topo;
+    *topo = novo;
+  } else {
+    node *atual = *topo;
+
+    while (atual->prox != NULL && atual->prox->valor < valor) {
+      atual = atual->prox;
+    }
+
+    novo->prox = atual->prox;
+    atual->prox = novo;
+  }
+}
+
+int excluir(node **topo, int valor) {
+  if (*topo == NULL) return -1;
+  
+  node *atual = *topo;
+
+  if (atual->valor == valor) {
+    *topo = atual->prox;
+    free(atual);
+    return 1;
+  }
+  
+  while (atual->prox != NULL && atual->prox->valor != valor)
+    atual = atual->prox;
+  
+  if (atual->prox == NULL) return -1;
+
+  node *remover = atual->prox;
+  atual->prox = remover->prox;
+  free(remover);
+  return 1;
 }
